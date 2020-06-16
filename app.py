a23f06f8fe0be10863aa77625c0e315a8e75d774
novandev <novanadams35@gmail.com>
2020-06-16 10:52:42 -0700

    addcors heaer in app config

--- app.py
+++ app.py
@@ -70,8 +70,8 @@ def send_contact_email_yagmail(email,subject,text):
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     print(response)
     return response
 
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
 from app import app
 from flask_cors import CORS
-CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 if __name__ == "__main__":
